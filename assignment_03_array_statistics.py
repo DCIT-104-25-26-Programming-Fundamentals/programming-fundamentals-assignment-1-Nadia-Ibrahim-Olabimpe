84561b7b0a3d5269b069d710a4aca49bf0381af1
NIO_81720 <nadiasparks843@gmail.com>
2026-07-28 11:25:29 +0000

    assignment 03 done

--- assignment_03_array_statistics.py
+++ assignment_03_array_statistics.py
@@ -39,3 +39,75 @@
 # YOUR CODE BELOW — remove the # symbols from the scaffold and fill it in
 # =============================================================================
 
+#Ask user for the number of numbers
+# Ask numbers from the user
+#Find sum, Average, Max and Min without using the built-in functions
+
+number_of_num= int(input("How many numbers?"))
+
+if number_of_num <= 0:
+    print("Error: Number of numbers must be a positive integer.")
+
+else:
+    user_numbers= []
+
+    for i in range (1,number_of_num + 1):
+        ask_user= int(input(f"Enter number {i}: "))
+
+    user_numbers.append(ask_user)
+
+   # print(user_numbers)
+
+def sum_numbers(user_numbers):
+    total=0
+    for i in range (len(user_numbers)):
+        total= total + user_numbers[i]
+
+    return total
+total=sum_numbers(user_numbers)
+print(f"Sum: {total}")
+
+
+
+def average_numbers(user_numbers):
+    total=sum_numbers(user_numbers)
+    mean= total/number_of_num
+
+    return mean
+
+mean=average_numbers(user_numbers)
+print(f"Average: {mean}")
+
+
+def max_numbers(user_numbers):
+    maximum=0
+    for i in range (len(user_numbers)):
+        if maximum < user_numbers[i]:
+            maximum = user_numbers[i]
+
+    return maximum
+
+maximum=max_numbers(user_numbers)
+print(f"Maximum: {maximum}")
+
+
+def min_numbers(user_numbers):
+    minimum=user_numbers[0]
+    for i in range (len(user_numbers)):
+       if minimum > user_numbers[i]:
+                  minimum = user_numbers[i]
+      
+
+    return minimum
+
+minimum=min_numbers(user_numbers)
+print(f"Minimum: {minimum}")
+
+   
+
+
+    
+
+
+
+    
